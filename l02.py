0b6a75f1ddfc57200ae8b479efcd6a100ba5da66
Kate <2003katy2003@mail.ru>
2023-03-03 19:03:44 +0300

    .

--- l02.py
+++ l02.py
@@ -7,8 +7,7 @@ from sklearn.datasets import load_iris
 iris_dataset = load_iris()
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(
-iris_dataset['data'], iris_dataset['target'], random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
 
 print("форма массива X_train: {}".format(X_train.shape))
 print("форма массива y_train: {}".format(y_train.shape))
@@ -17,10 +16,8 @@ print("форма массива y_test: {}".format(y_test.shape))
 
 
 iris_dataframe = pds.DataFrame(X_train, columns=iris_dataset.feature_names)
-
 from pandas.plotting import scatter_matrix
-grr = scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
-            hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+grr = scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
 
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors=1)
@@ -38,8 +35,7 @@ print("Прогнозы для тестового набора:\n {}".format(y_p
 print("Правильность на тестовом наборе: {:.2f}".format(np.mean(y_pred == y_test)))
 print("Правильность на тестовом наборе: {:.2f}".format(knn.score(X_test, y_test)))
 
-X_train, X_test, y_train, y_test = train_test_split(
-iris_dataset['data'], iris_dataset['target'], random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(X_train, y_train)
 print("Правильность на тестовом наборе: {:.2f}".format(knn.score(X_test, y_test)))
